f1ae76f4f680e083cdd8c4b519ba58d438cdce47
KacperS14275 <14275@eans-nt.edu.pl>
2026-05-13 15:32:53 +0200

    13.05.26 KS

--- home_library/home_library/urls.py
+++ home_library/home_library/urls.py
@@ -17,7 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from manage_books import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('manage_books.urls')),
+    path('', views.index, name='index'),
+    path('book/<int:book_id>/', views.book, name='book'),
+    path('author/<int:author_id>/', views.author, name='author'),
+    path('publisher/<int:publisher_id>/', views.publisher, name='publisher'),
+    path('series/<int:series_id>/', views.series, name='series'),
+    path('note/<int:note_id>/', views.note, name='note')
 ]
--- manage_books/views.py
+++ manage_books/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .models import Book, Author, Publisher, Series, Genre, Topic
 
 def index(request):
-    return HttpResponse("Hello from manage_books app NIGAAA")
+    # Pobieramy dane dla Scenariusza 1
+    context = {
+        'all_books': Book.objects.all(),
+        'favorite_books': Book.objects.filter(is_favorite=True),
+        'genres': Genre.objects.all(),
+        'topics': Topic.objects.all(),
+    }
+    return render(request, 'manage_books/index.html.jinja', context)
+
+def book(request, book_id):
+    book_obj = get_object_or_404(Book, id=book_id)
+    return render(request, 'manage_books/book.html.jinja', {'book': book_obj})
+
+def author(request, author_id):
+    author_obj = get_object_or_404(Author, id=author_id)
+    return render(request, 'manage_books/author.html.jinja', {'author': author_obj})
+
+def publisher(request, publisher_id):
+    pub_obj = get_object_or_404(Publisher, id=publisher_id)
+    return render(request, 'manage_books/publisher.html.jinja', {'publisher': pub_obj})
+
+def series(request, series_id):
+    series_obj = get_object_or_404(Series, id=series_id)
+    return render(request, 'manage_books/series.html.jinja', {'series': series_obj})
+
+def note(request, note_id):
+    # To zazwyczaj służy do edycji notatki, ale na razie wyświetlamy
+    return render(request, 'manage_books/note.html.jinja', {'note_id': note_id})
